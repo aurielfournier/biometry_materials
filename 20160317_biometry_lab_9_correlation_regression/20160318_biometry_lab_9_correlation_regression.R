6205fc988d379a2a63ad22d92113d3ff16956283
aurielfournier <aurielfournier@gmail.com>
2016-01-25 14:38:57 -0600

    ALL LABS MOVED TO R, just need more commenting

---
+++ 20160317_biometry_lab_9_correlation_regression/20160318_biometry_lab_9_correlation_regression.R
@@ -0,0 +1,39 @@
+# Biometry 2016 
+# Lab 9
+# Correlation and Regression
+
+
+# Using the data in "seasons.csv", answer the following questions.  
+# This file contains many abiotic and biotic variables that were collected in three different Ozark streams (see SITE).  
+# The following questions pertain to correlation and regression on variables in this dataset.
+
+#function for correlation
+# cor(x, y = NULL, method = c("pearson", "kendall", "spearman"))
+
+# fuction for a regression
+# lm(formula, data)
+
+# If you were interested in determining the degree of association among the following variables (Area, Cover, Canopy, Depth, Substrate, Totdens, Richness), how would you do it?
+
+# What is the difference between regression and correlation?  
+# Does the data meet the assumptions of the test?  
+# How can you tell?  
+# What could you do if the data did not meet the assumptions?
+
+
+# Carry out the appropriate test for the above data, assuming that they meet the assumptions of the test.  
+# Which correlations are significant?  
+# What does a significant correlation mean?  
+
+# For example, explain what the relationship is between substrate and depth, and between richness and totdens.  
+
+# Is there a problem with looking at so many tests (remember experimentwise and comparisonwise type I error rates)?
+
+
+# What are the assumptions of linear regression (see Quinn and Keough and my notes)?  
+# I want to know if Totdens and Richness can be predicted by Volume. 
+# Do the appropriate tests and report your results. 
+# What do they mean?  
+# Are the regressions significant?  
+# What does a significant regression mean?  
+# What are least squares?
